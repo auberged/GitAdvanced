8657b00499e9f7f252c7638f77c289801ff04644
auberged <56364045+auberged@users.noreply.github.com>
2019-10-15 16:47:07 +0200

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 int main() {
 	printf("Hello World\n");
 
-	printf("Es wurde eine Zeile hinzugefügt")
+	printf("Es wurde die Zeile im Master Branch direkt über das Github Repository editiert!")
 
 	return 0;
 }
